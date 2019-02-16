d01c079078117a06502e1e83819b6269a761e10e
JeanMick <nounahon.j@gmail.com>
2019-02-16 15:22:30 +0100

    Update Parsing data
    Parsing weight and parents

--- FeeEstimation.py
+++ FeeEstimation.py
@@ -1,7 +1,17 @@
+class MempoolTransaction():
+    def __init__(self, txid, fee, weight, parents):
+        self.txid = txid
+        self.fee = int(fee)
+        self.weight = int(weight)
+        self.parents = parents.strip().split(';')
+        # TODO: add code to parse weight and parents fields
 
+def parse_mempool_csv():
+    """Parse the CSV file and return a list of MempoolTransactions."""
+    with open('mempool.csv') as f:
+        return([MempoolTransaction(*line.strip().split(',')) for line in f.readlines()])
 
-
-
-if __name__ == "__main__" :
-
-    
+if __name__ ==  "__main__": 
+    a = parse_mempool_csv()
+    for i in a:
+        print (i.txid, i.fee, i.weight, i.parents)
