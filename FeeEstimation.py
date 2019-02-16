f8f107eff7948427797640ae627cb391bd5282c2
JeanMick <nounahon.j@gmail.com>
2019-02-16 16:30:14 +0100

    Update Description of the solution

--- FeeEstimation.py
+++ FeeEstimation.py
@@ -12,7 +12,36 @@ def parse_mempool_csv():
     with open('mempool.csv') as f:
         return([MempoolTransaction(*line.strip().split(',')) for line in f.readlines()])
 
+# I Understansd the Question:
+# --> We have an optimization problem where we need to maximum total fee for a miner by taking
+#      into account two constraints:
+
+# ----------> 1) Size of Block <= 4.000.000
+# ----------> 2) A transaction which possessess parents may appear in a Block on if 
+# ----------> 2) if all of its parents appear earlier in the block.
+
+# II Craft a Solution:
+# II.1 Craft a  naive solution(brute-force):
+#   ----> 1) Naive Solution : Search to calculate all combinaisons while taking into account the parents 
+#   ----> 1) insertion
+#   --------> Too many Case to make even without the parents Insertions (5214*5214) and before each intertion test if the weight is above 4.000.000 and insert parents transactions
+#   --------> The complexity we have is like k*p + 2^n with n number of items and k*p some particular values concerning the parents search insertion so z * 2^n as a complexity
+#Conclusion
+# ------> problem similar to Knapsack Problem with two conditions and one constraint. Let's use 
+
+# II.2 Craft a Solution:
+#   ----> 1) Naive Solution : Search to calculate all combinaisons while taking into account the parents 
+#   ----> 1) insertion
+#   --------> Too many Case to make even without the parents Insertions (5214*5214) and before each intertion test if the weight is above 4.000.000 and insert parents transactions
+#   --------> The complexity we have is like k*p + 2^n with n number of items and k*p some particular values concerning the parents search insertion so z * 2^n as a complexity
+#Conclusion
+# ------> problem similar to Knapsack Problem with two conditions and one constraint: 
+
+
+
 if __name__ ==  "__main__": 
+
     a = parse_mempool_csv()
-    for i in a:
-        print (i.txid, i.fee, i.weight, i.parents)
+    Fees = [i.fee for i in a]
+    Weight = [i.weight for i in a]
+    print(len(Fees))
